5d3830008d8690bbdfc25e504770bd0f103db264
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-11-28 02:30:05 -0300

    Fix - Adjustments

--- atv10/crud_alvos/crud_alvos/urls.py
+++ atv10/crud_alvos/crud_alvos/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('targets/', include('targets.urls')),
+    path('targets/', include('targets.api.v1.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- atv10/crud_alvos/targets/templates/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Target
-
-class TargetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Target
-        fields = '__all__'
