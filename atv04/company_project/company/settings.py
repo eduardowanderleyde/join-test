70f07d44c727abd0b65c2de3cba33a862c93c5df
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-11-28 10:14:03 -0300

    fix secret security

--- atv04/company_project/company/settings.py
+++ atv04/company_project/company/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -19,7 +19,8 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
