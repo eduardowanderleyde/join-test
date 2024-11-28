a70b436a7869740c362931a2ef0ac958b27e9daf
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-11-28 10:53:53 -0300

    Resolvendo conflitos de merge e removendo arquivos

--- atv04/company_project/queries/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('query-a/', views.query_a, name='query_a'),
-    path('query-b/', views.query_b, name='query_b'),
-    path('query-c/', views.query_c, name='query_c'),
-]
