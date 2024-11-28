734d46fcf7f940795368ca92acfd832ce7faa379
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-11-27 23:36:06 -0300

    Fixing the answers, second time

--- atv04/company_project/company/settings.py
+++ atv04/company_project/company/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "company.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'company_project',  
+        'USER': 'wander',  
+        'PASSWORD': 'join', 
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
